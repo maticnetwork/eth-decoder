661dc79cd2bb2e15d8397fc8e50246e7f0885326
Jaynti Kanani <jdkanani@gmail.com>
2018-09-21 01:10:36 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # eth-log-decoder
+
 Simple library to decode ethereum logs
+
+### Install
+
+```bash
+$ npm install eth-log-decoder
+```
+
+### Usage
+
+```js
+import LogDecoder from "eth-log-decoder";
+
+const ERC20TokenABI = /* { .... } */ // ABI for ERC20 token contract
+
+// create decoder object
+const decoder = new LogDecoder(
+  [
+    ERC20TokenABI,
+    // ... other ABIs
+  ]
+);
+
+// parse logs
+const parsedLogs = decoder.decodeLogs(receipt.logs) // For truffle testsuite, use `receipt.receipt.logs`
+console.log(parsedLogs)
+```
+
+### License
+
+MIT
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/* global __dirname, require, module */
+const path = require("path")
+const packageJSON = require("./package.json")
+
+const libraryName = packageJSON.name
+const config = {
+  mode: "production",
+  entry: `${__dirname}/src/index.js`,
+  devtool: "source-map",
+  output: {
+    path: `${__dirname}/lib`,
+    filename: `${libraryName}.js`,
+    library: libraryName,
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : this"
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js)$/,
+        loader: "babel-loader",
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve("./node_modules"), path.resolve("./src")],
+    extensions: [".json", ".js"]
+  }
+}
+
+module.exports = config
