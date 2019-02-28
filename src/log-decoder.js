8ca5dff863e41d9b1a8046354167d4e42f67a5fc
Jaynti Kanani <jdkanani@gmail.com>
2019-02-28 18:11:53 -0300

    fix log decoder

--- src/log-decoder.js
+++ src/log-decoder.js
@@ -5,15 +5,19 @@ export default class LogDecoder extends Decoder {
     return logs.map(log => {
       for (let i = 0; i < this._interfaces.length; i++) {
         const iface = this._interfaces[i]
-        const parsedLog = iface.parseLog(log)
-        if (parsedLog) {
-          return {
-            // parsedArgs,
-            address: log.address.toLowerCase(),
-            ...parsedLog,
-            event: parsedLog.name,
-            args: parsedLog.values
+        try {
+          const parsedLog = iface.parseLog(log)
+          if (parsedLog) {
+            return {
+              // parsedArgs,
+              address: log.address.toLowerCase(),
+              ...parsedLog,
+              event: parsedLog.name,
+              args: parsedLog.values
+            }
           }
+        } catch (e) {
+          // ignore
         }
       }
 
