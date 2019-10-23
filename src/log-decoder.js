f2b21bc1c4d5bda670c46ddd2e12b408a0b2d4bb
Jaynti Kanani <jdkanani@gmail.com>
2019-10-23 15:32:05 +0530

    allow partial logs decoding

--- src/log-decoder.js
+++ src/log-decoder.js
@@ -2,26 +2,28 @@ import Decoder from "./decoder"
 
 export default class LogDecoder extends Decoder {
   decodeLogs(logs = []) {
-    return logs.map(log => {
-      for (let i = 0; i < this._interfaces.length; i++) {
-        const iface = this._interfaces[i]
-        try {
-          const parsedLog = iface.parseLog(log)
-          if (parsedLog) {
-            return {
-              // parsedArgs,
-              address: log.address.toLowerCase(),
-              ...parsedLog,
-              event: parsedLog.name,
-              args: parsedLog.values
+    return logs
+      .map(log => {
+        for (let i = 0; i < this._interfaces.length; i++) {
+          const iface = this._interfaces[i]
+          try {
+            const parsedLog = iface.parseLog(log)
+            if (parsedLog) {
+              return {
+                // parsedArgs,
+                address: log.address.toLowerCase(),
+                ...parsedLog,
+                event: parsedLog.name,
+                args: parsedLog.values
+              }
             }
+          } catch (e) {
+            // ignore
           }
-        } catch (e) {
-          // ignore
         }
-      }
 
-      throw new Error("Log doesn't match")
-    })
+        return null
+      })
+      .filter(obj => obj != null)
   }
 }
